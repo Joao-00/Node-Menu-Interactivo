a00c8a5347667c7af95c6d33d4f6ef18a6108f5d
Joao Ormeño <joao23.1996@gmail.com>
2022-04-04 18:27:41 -0400

    Node: Se realiza esqueleto de menu interactivo

---
+++ app.js
@@ -0,0 +1,26 @@
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+require('colors');
+
+console.clear();
+
+const main = async() => {
+
+    console.log('Hola mundo');
+
+    let opt = '';
+
+    do {
+        opt = await mostrarMenu();
+        console.log({opt});
+
+        if (opt !== '0') await pausa();
+    } while (opt !== '0');
+
+
+}
+
+
+
+
+main();
